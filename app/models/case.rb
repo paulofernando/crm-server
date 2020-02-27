b3ee070aa8342da75bd5e007f2157e56a3b8e5f5
Paulo Fernando <pfernando@valore.com>
2020-02-27 18:23:22 -0300

    Add validation to case and contacts

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,3 +1,7 @@
 class Case < ApplicationRecord
     has_many :contacts
+    validates_associated :contacts
+    validates :title, presence: true
+    validates :title, uniqueness: { message: "title should be unique, please try another one" }
+    validates :value, numericality: { greater_than: 0 }
 end
