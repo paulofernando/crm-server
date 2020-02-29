9e0490b2c3aadf198b9d3269a3f8400cde42176e
Paulo Fernando <pfernando@valore.com>
2020-02-28 22:27:31 -0300

    Add UpdateContact mutation

--- app/graphql/mutations/create_contact.rb
+++ app/graphql/mutations/create_contact.rb
@@ -1,25 +1,27 @@
-class Mutations::CreateContact < Mutations::BaseMutation
+module Contacts
+  class Mutations::CreateContact < Mutations::BaseMutation
     argument :first_name, String, required: true
-    argument :last_name, String, required: true    
+    argument :last_name, String, required: true
     argument :case_role, Types::Enums::Role, required: true
     argument :email, String, required: true
     argument :case_id, ID, required: true
-  
+
     field :contact, Types::ContactType, null: false
     field :errors, [String], null: false
-  
+
     def resolve(first_name:, last_name:, case_role:, email:, case_id:)
-        contact = Contact.new(first_name: first_name, last_name: last_name, case_role: case_role, email: email, case_id: case_id)
-        if contact.save
-            {
-              contact: contact,
-              errors: []
-            }
-          else
-            {
-              contact: nil,
-              errors: contact.errors.full_messages
-            }
+      contact = Contact.new(first_name: first_name, last_name: last_name, case_role: case_role, email: email, case_id: case_id)
+      if contact.save
+        {
+          contact: contact,
+          errors: [],
+        }
+      else
+        {
+          contact: nil,
+          errors: contact.errors.full_messages,
+        }
       end
     end
   end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_contact, mutation: Mutations::CreateContact
+    field :update_contact, mutation: Mutations::UpdateContact
     field :create_case, mutation: Mutations::CreateCase
     field :update_case, mutation: Mutations::UpdateCase
   end
