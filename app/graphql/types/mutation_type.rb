547dd6c197448aff9f01e326135a292eab4b9241
Paulo Fernando <pfernando@valore.com>
2020-02-28 22:19:05 -0300

    Add UpdateCase mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_contact, mutation: Mutations::CreateContact
     field :create_case, mutation: Mutations::CreateCase
+    field :update_case, mutation: Mutations::UpdateCase
   end
 end
--- spec/factories/cases.rb
+++ spec/factories/cases.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     title { "MyString" }
     description { "MyString" }
     value { "9.99" }
-    court_date { "2020-02-28 12:28:27" }
+    court_date { "2020-02-28 15:38:19" }
   end
 end
