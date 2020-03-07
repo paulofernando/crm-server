897c4046bf3f7f8ba623be07a3ca8a8b51fbd402
Paulo Fernando <pfernando@valore.com>
2020-03-07 14:34:02 -0300

    Add custom method validation for Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,13 @@
 class Contact < ApplicationRecord
   belongs_to :court_case, optional: true
+  validate :court_case_cannot_have_contacts_with_same_role
+
+  def court_case_cannot_have_contacts_with_same_role
+    if court_case_id.present?
+      court_case = CourtCase.find(court_case_id)
+      if (court_case.contacts.where(:case_role => case_role).exists?)
+        errors.add(:case_role, "Another contact with same role is already attached to case")
+      end
+    end
+  end
 end
