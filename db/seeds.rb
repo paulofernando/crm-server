1a998a780d7fc365acdfa399b333f3100d6eb133
Paulo Fernando <pfernando@valore.com>
2020-03-07 14:51:43 -0300

    Modify seeds to create court case description between 50 and 150 words

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 5.times do
-  court_case = CourtCase.create(title: Faker::Lorem.sentence(word_count: 3), description: Faker::Lorem.sentence(word_count: 30), value: Faker::Number.decimal(l_digits: 2),
+  court_case = CourtCase.create(title: Faker::Lorem.sentence(word_count: 3), description: Faker::Lorem.sentence(word_count: rand(50..150)), value: Faker::Number.decimal(l_digits: 2),
                                 court_date: Faker::Date.forward(days: 100))
 
   court_case.contacts.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, case_role: "Accused", email: Faker::Internet.email)
