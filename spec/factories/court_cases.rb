7ec8bdff06aff705846c4a2fad87c89551dd923b
Paulo Fernando <pfernando@valore.com>
2020-03-12 13:14:21 -0300

    Change Factory data

--- spec/factories/court_cases.rb
+++ spec/factories/court_cases.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :court_case do
-    title { "MyString" }
-    description { "MyString" }
-    value { "9.99" }
+    title { "Title 1" }
+    description { "Description 1" }
+    value { "10.00" }
     court_date { "2020-03-01 14:17:13" }
   end
 end
